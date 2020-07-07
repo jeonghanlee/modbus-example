c8362203a8daf53b0a8d6e2a37f3f8014f62fa4a
Jeong Han Lee <jeonghan.lee@gmail.com>
2020-07-06 17:15:41 -0700

    update ...

--- st.cmd
+++ st.cmd
@@ -1,22 +1,18 @@
 #!/home/train/build-epics/modbus/bin/linux-x86_64/modbusApp
 
-epicsEnvSet("TOP","${PWD}")
-epicsEnvSet("DB_TOP", "$(TOP)/db")
-epicsEnvSet("IOCSH_TOP", "$(TOP)/iocsh")
+epicsEnvSet("CMDTOP","${PWD}")
+epicsEnvSet("DB_TOP", "$(CMDTOP)/db")
+
 
 epicsEnvSet("LOCATION","TESTLAB")
-
 
 epicsEnvSet("SYSSUBSYS","LBNL")
 epicsEnvSet("DEVDI", "MODBUSIOC")
 epicsEnvSet("IOCNAME", "$(SYSSUBSYS):$(DEVDI)")
 epicsEnvSet("USERNAME", "train")
 
-
-
-
 #-# Register all support components
-dbLoadDatabase "dbd/modbus.dbd"
+dbLoadDatabase "$(CMDTOP)/dbd/modbus.dbd"
 modbus_registerRecordDeviceDriver pdbbase
 
 #- Use the following commands for TCP/IP
@@ -44,9 +40,14 @@ modbusInterposeConfig("$(PORTNAME)","$(LINKTYPE=0)", "$(TIMEOUT=5000)", "$(WRITE
 # Enable ASYN_TRACEIO_HEX on octet server
 asynSetTraceIOMask("$(PORTNAME)",0,4)
 # Enable ASYN_TRACE_ERROR and ASYN_TRACEIO_DRIVER on octet server
-#asynSetTraceMask("sim1",0,9)
+asynSetTraceMask("$(PORTNAME)",0,9)
 
-#dbLoadTemplate("sim1.substitution")
+# Bit access at Modbus address 0
+# Access 128 bits as inputs.  
+# Function code=1
+# drvModbusAsynConfigure("portName", "tcpPortName", slaveAddress, modbusFunction, modbusStartAddress, modbusLength, dataType, pollMsec, "plcType")
+#drvModbusAsynConfigure("A0_In_Bits", "$(PORTNAME)", 0, 1,  0, 128, 0, 100, "Simulator")
+dbLoadTemplate("$(DB_TOP)/modbus1.substitutions", "DB_TOP=$(DB_TOP), IOCNAME=$(IOCNAME)")
 
 
 iocInit()
